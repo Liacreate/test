3ce9db5c66731d0bfee072417bd8212073793fc2
Liacreate <scifiziz@gmail.com>
2021-05-22 15:22:03 +0800

    test

---
+++ 002/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+
+namespace _002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var x = int.Parse(Console.ReadLine());
+
+            var a = Enumerable.Range(1, x).Reverse();
+
+            int i = 0;
+
+            foreach (var item in a)
+            {
+                i++;
+                var b = Enumerable.Repeat(item, i);
+                foreach (var c in b)
+                {
+                    Console.Write(c);
+                }
+                Console.WriteLine();
+            }
+
+          
+
+
+
+            //Console.WriteLine(
+            //string.Join(
+            //    "\n",
+            //    Enumerable.Range(1, int.Parse(Console.ReadLine()))
+            //    .Reverse()
+            //    .Select((x, index) => string.Concat(Enumerable.Repeat(x, index + 1)))
+
+            //    ));
+
+
+
+
+
+
+
+
+            //Console.WriteLine(Plus.Pattern(x));
+
+
+            Console.ReadLine();
+
+        }
+
+        //public static class Plus
+        //{
+        //    public static string Pattern(int n)
+        //    {
+        //        return n < 1 ? "" : string.Join("\n",
+        //            Enumerable
+        //            .Range(1, n)
+        //            .Select(i => string.Join("", Enumerable.Repeat(i, i))));
+        //    }
+        //}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string number = Console.ReadLine();
+            //string[] x = number.Split(',');
+
+            string[] x = Console.ReadLine().Split(',');
+
+           var a = x.Reverse();
+  
+            Console.WriteLine($"{string.Join((","), a)}");
+
+
+            //List<int> _list = new List<int>();
+
+            //foreach (var y in x)
+            //{
+            //    int z = int.Parse(y);
+            //    _list.Add(z);
+            //}
+
+            //Console.WriteLine($"{string.Join((","), _list)}");
+
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
